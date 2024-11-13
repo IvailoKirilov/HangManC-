26d889f8ec1f9f3f5abaeafeff3808c25fac0335
gogo1701 <30569429+gogo1701@users.noreply.github.com>
2024-11-13 10:41:30 +0200

    Changed structure

---
+++ Code/HangmanGame/HangmanGame/Classes/Words.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanGame.Classes
+{
+    internal class Words
+    {
+        public string getRandomWord()
+        {
+            string line = null;
+            StreamReader sr = new StreamReader("C:\\Sample.txt");
+            List<string> words = new List<string>();
+
+            while (line != null)
+            {
+                line = sr.ReadLine();
+
+            }
+
+            return;
+        }
+    }
+}
