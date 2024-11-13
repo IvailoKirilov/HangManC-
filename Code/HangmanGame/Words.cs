a1de8dedcbcc16bbc1bf2fd38afbf0c77501bd31
gogo1701 <30569429+gogo1701@users.noreply.github.com>
2024-11-13 10:38:16 +0200

    Added solution and basic files

---
+++ Code/HangmanGame/Words.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		public string getRandomWord()
+		{
+            StreamReader sr = new StreamReader("C:\\Sample.txt");
+
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                //Read the next line
+                line = sr.ReadLine();
+            }
+        }
+    }
+}
