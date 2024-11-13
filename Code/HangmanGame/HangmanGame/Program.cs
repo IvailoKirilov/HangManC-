3623f1864df0388b1c96b0ce86949a6c68cd2594
IvailoKirilov <ivaylo.kirilov.highschool@buditel.bg>
2024-11-13 10:52:57 +0200

    Started Game Logic

--- Code/HangmanGame/HangmanGame/Program.cs
+++ Code/HangmanGame/HangmanGame/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void DrawCurrentGameState(bool inputIsInvalid, int incorrectGuess, string guessedWord, List<char> playerUsedLetters)
+{
+
+}
+
+while (true)
+{
+    string word = GetRandomWord(Words);
+    string wordToGuess = new(Underscore, word.Lenght);
+
+    int incorrectGuessCount = 0;
+    List<char> playerUsedLetters = new Lest<char>();
+
+    
+}
